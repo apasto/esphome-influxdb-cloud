6e2a0ad1cc0129b712bde2952ccec833255b576f
f4n <f4n@users.noreply.github.com>
2026-02-27 17:08:37 +0100

    Add Network library for ESP32 support

--- components/influxdb/__init__.py
+++ components/influxdb/__init__.py
@@ -40,6 +40,7 @@ async def to_code(config):
     cg.add(var.set_measurement(config[CONF_INFLUXDB_MEASUREMENT]))
     
     if CORE.is_esp32:
+        cg.add_library('Network', None)
         cg.add_library('NetworkClientSecure', None)
         cg.add_library('HTTPClient', None)
     if CORE.is_esp8266:
